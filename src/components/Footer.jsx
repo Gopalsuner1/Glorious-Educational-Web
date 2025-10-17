9be1a2faed8f07d397c7418cb228918cbac3f116
Gopalsuner1 <gopalsuner111@gmail.com>
2025-10-16 21:02:35 -0700

    new changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,19 +3,19 @@ import { FiPhoneCall } from "react-icons/fi";
 import { FaMapMarkerAlt } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className="w-full md:pl-[90px] md:pr-[90px] pt-3 rounded-2xl h-[350px] flex flex-col md:flex-row md:flex-wrap  bg-[#96A78D]">
-      <div className=" md:h-[80%] h-[50%]  flex md:flex-col md:w-1/2 w-full md:gap-4">
-        <div className="flex md:gap-6 gap-2 md:w-full w-1/2 ">
-          <img className="md:w-26 md:h-26 h-13 w-13" src={logo} alt="school logo" />
+    <div className="w-full h-fit gap-5 md:gap-0 md:pl-[90px] md:pr-[90px] pt-3 md:rounded-2xl md:h-[350px] flex flex-col md:flex-row md:flex-wrap  bg-[#96A78D]">
+      <div className=" md:h-[80%]  h-[50%] pl-4 flex flex-col md:w-1/2 w-full md:gap-6 gap-4">
+        <div className="flex md:gap-6 items-center pl-4 md:pl-0 gap-2 w-full ">
+          <img className="md:w-26 md:h-26 h-11 w-11" src={logo} alt="school logo" />
           <div>
-            <h1 className="font-bold md:text-[39px] text-[15px] ">Glorious</h1>
-            <h4 className="font-bold md:text-[21px] text-[11px] ">
+            <h1 className="font-bold md:text-[39px] text-[17px] ">Glorious</h1>
+            <h4 className="font-bold text-nowrap md:text-[21px] text-[13px] ">
               Educational Academy School
             </h4>
           </div>
         </div>
-        <div className="flex md:gap-6 gap-1.5  pl-5 items-center">
-          <FaMapMarkerAlt className="md:text-[29px] text-[11px] " />
+        <div className="flex md:gap-6 gap-5  pl-5 items-center">
+          <FaMapMarkerAlt className="md:text-[29px] text-[18px] " />
           <div>
             <h4>Glorious Educational Academy School, Dudhiya,</h4>
             <h4>खसरा न. - 2/8/2 Village - दूधिया पीतांबरा धाम मंदिर रोड</h4>
@@ -27,14 +27,14 @@ const Footer = () => {
           <h1 className=" text-[15px]">+91 9977773630 | +91 9009090245</h1>
         </div>
       </div>
-      <div className=" h-[80%] w-1/2 pt-6 pl-5 grid grid-cols-3 grid-rows-5 gap-4 text-nowrap text-justify">
-            <h1 className="font-bold text-2xl text-nowrap  col-start-1 row-start-1 ">QUICK LINKS</h1>
+      <div className=" h-[80%]  md:text-[11px] text-[14px]  md:w-1/2 w-full md:pt-6 pl-8 grid grid-cols-3 grid-rows-5 gap-4 text-nowrap text-justify">
+            <h1 className="font-bold md:text-2xl text-[13px] text-nowrap  col-start-1 row-start-1 ">QUICK LINKS</h1>
             <a className="col-start-1 row-start-2 w-fit font-semibold" href="">Our Journey</a>
             <a className="col-start-1  row-start-3 w-fit font-semibold" href="">Admission Process</a>
             <a className="col-start-1  row-start-4 w-fit font-semibold" href="">Boarding FAQ</a>
-            <a className="col-start-1  row-start-5 w-fit font-semibold" href="">Boarding Experience</a>
+            <a className="col-start-2  row-start-2 w-fit font-semibold" href="">Boarding Experience</a>
             <a className="col-start-2  row-start-3 w-fit font-semibold" href="">Learning Support</a>
-            <a className="col-start-2  row-start-2 w-fit font-semibold" href="">Contact Us</a>
+            <a className="col-start-2  row-start-4 w-fit font-semibold" href="">Contact Us</a>
 
       </div>
 
--- src/components/Poster.jsx
+++ src/components/Poster.jsx
@@ -3,9 +3,9 @@ import style from '../styles/Home.module.css'
 const Poster = () => {
   return (
     <div className={` md:w-[95%] md:p-[50px] shadow-lg shadow-black items-center  rounded-2xl overflow-hidden w-[95%] md:h-[560px] h-[250px] ${style.bgdiv}  flex flex-col justify-center `}>
-                  <div className={` flex gap-6 overflow-hidden flex-col w-full md:p-14 justify-center items-center  ${style.anim1}`}>
-                     <h1 className={`md:text-[42px] text-[#ffff] text-[22px] mt-5 font-bold ${style.f1 } `}> Glorious Educational Academy</h1>
-                     <p className='text-[11px] md:text-[16px] text-[#ffff] '>Founded in 2015, Glorious Educational Academy is a premier co-educational English medium school located in Indore. Affiliated with the M.P. Board, Bhopal, and following the CBSE pattern, we combine academic rigor with a nurturing environment to prepare students for success in every field of life.
+                  <div className={` flex md:gap-6 gap-2 overflow-hidden flex-col w-full md:p-14 justify-center items-center  ${style.anim1}`}>
+                     <h1 className={`md:text-[42px] text-[#ffff] text-[19px] mt-5 font-bold ${style.f1 } `}> Glorious Educational Academy</h1>
+                     <p className='text-[9px] md:text-[16px] text-[#ffff] '>Founded in 2015, Glorious Educational Academy is a premier co-educational English medium school located in Indore. Affiliated with the M.P. Board, Bhopal, and following the CBSE pattern, we combine academic rigor with a nurturing environment to prepare students for success in every field of life.
 
 Our students consistently shine in academics and sports, earning accolades and recognition at various levels. Behind their success is a team of passionate educators, committed administrative staff, and a strong support system of parents and well-wishers who believe in our vision.
 
