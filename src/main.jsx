781b77ff0719cd34d6d0d9ccd1c23f9fab759e3d
Gopalsuner1 <gopalsuner111@gmail.com>
2025-10-31 16:05:09 +0530

    ns

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Lenis from "@studio-freight/lenis";
-import { BrowserRouter } from "react-router-dom";
 
 const lenis = new Lenis({
   duration: 1.2,
@@ -19,8 +18,6 @@ requestAnimationFrame(raf);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
     <App />
-    </BrowserRouter>
   </StrictMode>
 );
